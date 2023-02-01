fa01c0597f8959ebe4d41c55127f26b41fab43f0
mauricior-banega <100450611+mauricior-banega@users.noreply.github.com>
2023-02-01 20:23:02 -0300

    Desde 02_02_03 (solo este video)
    -En este práctico veremos que en PHP podemos utilizar números de distintos tipos: enteros/decimales/notación científica etc.

---
+++ C2 02-03/enteros.php
@@ -0,0 +1,33 @@
+<!--
+
+    12-PHP Básico (Video2Brain) - Todos los videos estan en carpeta: DVD/9430830/xxdata
+
+Videos/Temas contenidos del Dashboard: 
+    -Desde 02_02_03 (solo este video)
+
+-En este práctico veremos que en PHP podemos utilizar numeros de distintos tipos: enteros/decimales/notacion cientifica etc.
+
+ACLARACION: Para expresar decimales hay que poner un punto luego del numero, en vez de la coma.
+
+-->
+
+<?php
+
+    $entero = 5;
+
+    echo $entero;
+
+    echo "<br>";
+
+    $decimal = 5.432;
+
+    echo $decimal;
+
+    echo "<br>";
+
+    $cientifica = 0.123e2; //Exponencial: 0.123 EXP 2
+
+    echo $cientifica;
+
+?>
+
