4651f759e3cc9a0e1a6e6769e1cfe39a72252685
mauricior-banega <100450611+mauricior-banega@users.noreply.github.com>
2023-02-01 19:20:18 -0300

    Desde 02_01_01 al 02_01_03
    Damos comienzo al practico: Creamos un archivo echo.php del cual explica conceptos básicos en cuanto a su lógica y sintaxis. Además de como convive la estructura PHP dentro de HTML.

---
+++ echo.php
@@ -0,0 +1,36 @@
+<!--
+
+    12-12-PHP Básico (Video2Brain) (VideotuBrain) - Todos los videos estan en carpeta: DVD/9430830/xxdata
+
+Videos/Temas contenidos del Dashboard: 
+    -Desde 02_01_01 al 02_01_03
+
+-En este practico veremos un archivo echo.php. Del cual su estructura es la de HTML, es decir conviven tambien en cuanto a la creacion y funcionamiento de archivos.
+
+-
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Echo.php</title>
+</head>
+<body>
+    
+    <h1>Hola soy un título</h1>
+
+    <?php
+
+        //Esto es un comentario
+        /*Todo lo que este aqui dentro se concidera un comentario*/
+        echo "Hola soy un párrafo";
+        echo "<br>";
+        echo 3+3;
+
+    ?>
+
+</body>
+</html>
