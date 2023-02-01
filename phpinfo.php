653691c695847d88f6d6c504a7f6adb18074eb80
mauricior-banega <100450611+mauricior-banega@users.noreply.github.com>
2023-02-01 19:30:40 -0300

    Desde 02_01_04 (solo ese video)
    -En este practico creamos un archivo phpinfo.php. Del cual explica cual es la importancia de crearlo, ya que nos devuelve una tabla completa de las funcionalidades, lo que esta o no instalado en el servidor, que nos están sirviendo nuestras paginas web echas con PHP. Esto se realiza como veremos con una simple linea.

---
+++ phpinfo.php
@@ -0,0 +1,23 @@
+<!--
+
+    12-12-PHP Básico (Video2Brain) (VideotuBrain) - Todos los videos estan en carpeta: DVD/9430830/xxdata
+
+Videos/Temas contenidos del Dashboard: 
+    -Desde 02_01_04 (solo ese video).
+
+-En este practico creamos un archivo phpinfo.php. Del cual explica cual es la importancia de crearlo, ya que nos devuelve una tabla completa de las funcionalidades, lo que esta o no instalado en el servidor, que nos estan sirviendo nuestras paginas web echas con PHP. Esto se realiza como veremos cun una simple linea.
+
+-No sera necesario aplicar estructura HTML en esta actividad.
+
+-Pobremos ver todas las versiones de cada modulo instalado del servidor que corre. Sirve para que de manera que queramos saber o instalar algun modulo que no tengamos podramos verlo ejecutando este archivo y visualizandolo atravez de /localhost, en este mi caso la ruta será:
+
+    http://localhost/Estudios/12-PHP%20B%C3%A1sico%20(Video2Brain)/12-PHP-Basico-Video2Brain-/phpinfo.php
+
+
+-->
+
+<?php
+
+    phpinfo();
+
+?>
