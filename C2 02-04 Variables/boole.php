24957d5392364735fe4f35f0d21bfb467be507d9
mauricior-banega <100450611+mauricior-banega@users.noreply.github.com>
2023-02-01 20:31:41 -0300

    Desde 02_02_04 (solo este video)
    -En este práctico veremos que en PHP podemos utilizar variables Booleanas (true/false), donde en cada caso se traduce como true=1 y false=0 (vacía).

---
+++ C2 02-04 Variables/boole.php
@@ -0,0 +1,20 @@
+<!--
+
+    12-PHP Básico (Video2Brain) - Todos los videos estan en carpeta: DVD/9430830/xxdata
+
+Videos/Temas contenidos del Dashboard: 
+    -Desde 02_02_04 (solo este video)
+
+-En este práctico veremos que en PHP podemos utilizar variables Booleanas (true/false), donde en cada caso se traduce como true=1 y false=0 (vacía).s
+
+ACLARACION: 
+
+-->
+
+<?php
+
+    $booleana = true;
+
+    echo "El valor de la variable es" .$booleana;
+
+?>
