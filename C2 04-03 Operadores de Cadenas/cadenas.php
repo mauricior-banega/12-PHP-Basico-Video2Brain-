1c948c4864c23286df7b1571fe3ac8919b354d2b
mauricior-banega <100450611+mauricior-banega@users.noreply.github.com>
2023-02-02 17:50:36 -0300

    Desde 02_04_03 (solo este video)
    -En este práctico super corto veremos lo fácil que es encadenar "cadenas" en PHP. Como ya vimos el encadenamiento o concatenación en php se hace mediante el punto.

---
+++ C2 04-03 Operadores de Cadenas/cadenas.php
@@ -0,0 +1,26 @@
+<!--
+
+    12-PHP Básico (Video2Brain) - Todos los videos estan en carpeta: DVD/9430830/xxdata
+
+Videos/Temas contenidos del Dashboard: 
+    -Desde 02_04_03 (solo este video)
+
+-En este práctico super corto veremos lo facil que es encadenar "cadenas" en PHP. Como ya vimos el encadenamiento o concatenacion en php se hace mediante el pinto.
+
+ACLARACIÓN: 
+
+-->
+
+<?php
+
+
+$mi = "Mi nombre es ";
+$nombre = "Jose Vicente ";
+$saludo = "y yo te saludo.";
+
+echo $mi.$nombre.$saludo;
+
+//Imprimirá: Mi nombre es Jose Vicente y yo te saludo.
+
+
+?>
