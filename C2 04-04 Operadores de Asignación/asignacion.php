c287fcfad5604c45b11963bc2fe37044eee07a33
mauricior-banega <100450611+mauricior-banega@users.noreply.github.com>
2023-02-02 17:57:36 -0300

    Desde 02_04_04 (solo este video)
    -En este práctico veremos como los operadores de asignación (=,+=,-=,*=,/=).Nos permite asignar valores, en la mayoría de las casos a variables.

---
+++ C2 04-04 Operadores de Asignación/asignacion.php
@@ -0,0 +1,48 @@
+<!--
+
+    12-PHP Básico (Video2Brain) - Todos los videos estan en carpeta: DVD/9430830/xxdata
+
+Videos/Temas contenidos del Dashboard: 
+    -Desde 02_04_04 (solo este video)
+
+-En este práctico veremos como los operadores de asignacion (=,+=,-=,*=,/=).Nos permite asignar valores, en la mayoria de las casos a variables.
+
+ACLARACIÓN: 
+
+-->
+
+<?php
+
+
+$mivariable = 7;
+
+echo $mivariable; //Imprime 7
+
+echo "<br>";
+
+$mivariable += 5; //Imprime 7+5 = 12
+
+echo $mivariable;
+
+echo "<br>";
+
+$mivariable -= 5; //Imprime 12-5 = 7
+
+echo $mivariable;
+
+echo "<br>";
+
+$mivariable *= 5; //Imprime 7*5 = 35
+
+echo $mivariable;
+
+echo "<br>";
+
+$mivariable /= 5; //Imprime 35/5 = 7
+
+echo $mivariable;
+
+echo "<br>";
+
+
+?>
