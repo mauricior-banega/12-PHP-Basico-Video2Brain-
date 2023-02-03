1f8682df6989fd33c64872ae714ee092a834fa16
mauricior-banega <100450611+mauricior-banega@users.noreply.github.com>
2023-02-03 20:35:06 -0300

    -Desde 02_07_01 (solo este video)
    -En este práctico explicaremos como incluir mediante "include" un archivo PHP dentro de otro.
    
    -Lo que realiza en este caso es imprimir un texto en un archivo "origen.php" pero a la vez realizar un llamado a leer también lo que contiene el archivo "incluido.php" que es también un texto.

---
+++ C2 07-01 Origen/origen.php
@@ -0,0 +1,21 @@
+<!--
+
+    12-PHP Básico (Video2Brain) - Todos los videos estan en carpeta: DVD/9430830/xxdata
+
+Videos/Temas contenidos del Dashboard: 
+    -Desde 02_07_01 (solo este video)
+
+-En este práctico explicaremos como incluir mediante "include" un archivo PHP dentro de otro.
+
+-Lo que realiza en este caso es imprimir un texto en un archivo "origen.php" pero a la vez realizar un llamado a leer tambien lo que contiene el archivo "incluido.php" que es tambien un texto.
+
+ACLARACIÓN: 
+
+-->
+
+<?php
+
+echo "Yo soy el contenido original";
+include "incluido.php";
+
+?>
